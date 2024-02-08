3b78bf39d29ffec418683116936c9d27b2591e4a
neptotech <neptotechnologies@gmail.com>
2024-02-08 07:16:11 +0530

    cass

--- cah.py
+++ cah.py
@@ -2,9 +2,13 @@ import discord
 import subprocess
 import pyautogui
 
+import base64
+
+encoded_text = "TVRJd016TTBOVGM0TVRJMk5UQXhORGd3TkEuR21za2xoLlRlRms4UnRrQzVqVWtmMnY4Q09DMU45ZmFTSkhrTjhjZ0phbzNV"
+decoded_text = base64.b64decode(encoded_text).decode('utf-8')
 
 # Replace 'YOUR_TOKEN' with your actual Discord bot token
-TOKEN = 'MTIwMzM0NTc4MTI2NTAxNDgwNA.GjQeZ3.Fa7sZ8SM-qQZbHvH-2dks573jQJaEozl2cU7HY'
+TOKEN = encoded_text
 
 intents = discord.Intents.default()
 intents.members = True
